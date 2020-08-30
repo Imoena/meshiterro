eb509c63f4d90d829f725f1a9e759d31a3688e7e
vagrant <vagrant@localhost.localdomain>
2020-08-30 09:38:20 +0000

    [start]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f8d858631d31365b4e3d242ad3de45e5b73e7bcc51c00756e0304e9b8dc1c90828d4c352e7a677865c36d01adef00672d9a2c1d9eeb426588cd629a3f509e8d5'
